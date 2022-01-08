1e9ff1b677d2614ab2e8b57bc986b5c2e6797c3b
risonakamo <hmykanae@gmail.com>
2022-01-07 22:32:13 -0500

    hide timer display hidden state

--- web/components/hide-timer/hide-timer.svelte
+++ web/components/hide-timer/hide-timer.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+  import cx,{Mapping} from "classnames";
+
   import "./hide-timer.less";
+
+  const tempHiddenProp:boolean=false;
+
+  const hideTimerDisplayClass:Mapping={
+    hidden:tempHiddenProp
+  };
 </script>
 
 <div class="hide-timer">
   <h1>Timer 1</h1>
 
-  <div class="hide-timer-display">
-    <span class="digits">00</span>
-    <span class="unit">h</span>
-    <span class="digits">22</span>
-    <span class="unit">m</span>
-    <span class="digits">05</span>
-    <span class="unit">s</span>
+  <div class={cx("hide-timer-display",hideTimerDisplayClass)}>
+    <div class="inner">
+      <span class="digits">00</span>
+      <span class="unit">h</span>
+      <span class="digits">22</span>
+      <span class="unit">m</span>
+      <span class="digits">05</span>
+      <span class="unit">s</span>
+    </div>
   </div>
 
   <p class="action-info">
